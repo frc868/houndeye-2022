cb2aa4e4fcac23885a95a91201fdbbb727863dc6
davidracovan <74030080+superpenguin612@users.noreply.github.com>
2022-01-24 11:36:24 -0500

    Create config.py
    This will most likely be changed to a yml file in the future.

---
+++ frc_vision/config.py
@@ -0,0 +1,20 @@
+# Placeholder for now, does not work yet
+
+# ----CAMERA----
+
+
+class CameraTypes:
+    ASTRA = 0
+    WEBCAM = 1
+
+
+CAMERA = CameraTypes.ASTRA
+USE_RECORDING = False  # Set CAMERA to None when using this option
+
+
+# ----CALIBRATION----
+MASK_CALIBRATE = False
+HOUGH_CALIBRATE = False
+
+# ----NETWORKTABLES----
+ENABLE_NETWORKTABLES = False
