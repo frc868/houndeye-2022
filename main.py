625ef5fe04f8b5309265ad2b8313537019cab90d
davidracovan <74030080+superpenguin612@users.noreply.github.com>
2022-01-26 08:13:37 -0500

    Update main.py to use recording

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import frc_vision.astra.driver
+# import frc_vision.astra.driver
+import frc_vision.recording
+import frc_vision.webcam.driver
 
 if __name__ == "__main__":
-    d = frc_vision.astra.driver.Driver()
+    d = frc_vision.recording.Driver()
     d.run(view=True)
