231485a0be732387be8c664b00c7fc27e4e8f9db
davidracovan <74030080+superpenguin612@users.noreply.github.com>
2022-01-28 21:54:23 -0500

    Add find_circles to astra utils, and add documentation to all funcs

--- frc_vision/astra/utils.py
+++ frc_vision/astra/utils.py
@@ -9,6 +9,20 @@ from frc_vision.utils import circles, cv2Frame
 def generate_masks(
     frame: cv2Frame,
 ) -> tuple[cv2Frame, cv2Frame]:
+    """
+    Generates a blue and a red mask for a frame.
+    (applies GaussianBlur at the start)
+
+    Params:
+        frame: a raw frame from Astra
+
+    Returns:
+        blue_mask
+        red_mask
+
+    Raises:
+        None
+    """
     frame = cv2.GaussianBlur(frame, (7, 7), sigmaX=1.5, sigmaY=1.5)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -33,11 +47,94 @@ def generate_masks(
     return blue_mask, red_mask
 
 
+def find_circles(mask: cv2Frame, depth_frame: cv2Frame) -> circles:
+    """
+    Finds circles given a blurred mask.
+
+    Params:
+        mask: a BLURRED mask (typically GaussianBlur) to perform operations on
+        depth_frame: a depth frame from the Astra
+
+    Returns:
+        circles: circles found in the frame
+
+    Raises:
+        None
+    """
+
+    cnts, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    circles = []
+    for c in cnts:
+        ((x, y), r) = cv2.minEnclosingCircle(c)
+
+        circle_area = 3.14 * r ** 2
+        contour_area = cv2.contourArea(c)
+
+        circle_validators = [
+            r > 10,
+            (
+                (circle_area / contour_area)
+                < frc_vision.constants.CIRCLE_COMPARISON_THRESHOLD
+            )
+            if contour_area != 0
+            else False,
+        ]
+
+        if all(circle_validators):
+            d = calculate_distance(x, y, r, depth_frame)
+            circles += [(int(x), int(y), int(r), int(d))]
+
+    return circles
+
+
+def calculate_distance(x: int, y: int, r: int, depth_frame: cv2Frame) -> float:
+    """
+    Calculates the distance a certain circle is from the camera.
+
+    Params:
+        x: the x coordinate of the circle in the frame (NOT tx)
+        y: the y coordinate of the circle in the frame (NOT ty)
+        r: the radius of the circle
+        depth_frame: a depth frame from the Astra
+
+    Returns:
+        distance: the distance the ball is from the camera (needs calibration)
+
+    Raises:
+        None
+
+    """
+    roi_margin = int(
+        r * 0.20
+    )  # can be adjusted to reflect the portion of the ball to test
+    x1 = max(0, x - roi_margin)
+    x2 = min(frc_vision.constants.ASTRA.RESOLUTION_W - 1, x + roi_margin)
+    y1 = max(0, y - roi_margin)
+    y2 = min(frc_vision.constants.ASTRA.RESOLUTION_H - 1, y + roi_margin)
+    roi = depth_frame[y1:y2, x1:x2]
+    distance = np.average(roi)
+
+    return distance
+
+
 def calculate_angles(circles: circles):
-    """Calculates x and y deviation from center of camera feed."""
+    """
+    Calculates x and y deviation from center of camera feed.
+
+    Params:
+        circles: circles returned from `find_circles`
+
+    Returns:
+        tx: the x degree offset from the center of the frame
+        ty: the y degree offset from the center of the frame
+
+    Raises:
+        None
+    """
     tx = []
     ty = []
-    for x, y, r in circles:
+    for x, y, r, d in circles:
         tx += [
             (frc_vision.constants.ASTRA.FOV_H / 2)
             * (x - (frc_vision.constants.ASTRA.RESOLUTION_W / 2))
@@ -51,42 +148,10 @@ def calculate_angles(circles: circles):
     return tx, ty
 
 
-def calculate_distance(circles: circles, depth_frame: cv2Frame) -> list[float]:
-    ta = []
-    for x, y, r in circles:
-        roi_margin = int(
-            r * 0.20
-        )  # can be adjusted to reflect the portion of the ball to test
-        x1 = max(0, x - roi_margin)
-        x2 = min(frc_vision.constants.ASTRA.RESOLUTION_W - 1, x + roi_margin)
-        y1 = max(0, y - roi_margin)
-        y2 = min(frc_vision.constants.ASTRA.RESOLUTION_H - 1, y + roi_margin)
-        roi = depth_frame[y1:y2, x1:x2]
-
-        ta += [int(np.average(roi))]
-    return ta
-
-def calculate_distance(circles: circles, depth_frame: cv2Frame) -> list[float]:
-    ta = []
-    for x, y, r in circles:
-        roi_margin = int(
-            r * 0.20
-        )  # can be adjusted to reflect the portion of the ball to test
-        x1 = max(0, x - roi_margin)
-        x2 = min(frc_vision.constants.ASTRA.RESOLUTION_W - 1, x + roi_margin)
-        y1 = max(0, y - roi_margin)
-        y2 = min(frc_vision.constants.ASTRA.RESOLUTION_H - 1, y + roi_margin)
-        roi = depth_frame[y1:y2, x1:x2]
-        print(roi)
-
-        ta += [int(np.average(roi))]
-    return ta
-
-
-def zip_networktables_data(txb, tyb, txr, tyr, tab, tar):
+def zip_networktables_data(txb, tyb, txr, tyr, tdb, tdr):
     """Add color information and zip data for NetworkTables transmission."""
     color = ["B" for x in txb] + ["R" for x in txr]
     tx = txb + txr
     ty = tyb + tyr
-    ta = tab + tar
-    return color, tx, ty, ta
+    td = tdb + tdr
+    return color, tx, ty, td
