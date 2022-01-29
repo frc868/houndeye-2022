5e6c30099814e35ae8494d48b54aa53612e6c3c5
davidracovan <74030080+superpenguin612@users.noreply.github.com>
2022-01-28 20:40:26 -0500

    Update the driver station client with extra comments and revisions
    This will be updated later with error handling so that it doesn't hang when the raspi isn't on.

--- client.py
+++ client.py
@@ -11,7 +11,10 @@ import frc_vision.constants
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((frc_vision.constants.SERVER_IP, frc_vision.constants.SERVER_PORT))
 
-vcap = cv2.VideoCapture("http://limelight.local:5800")
+vcap = cv2.VideoCapture(
+    f"http://{frc_vision.constants.SERVERS.LIMELIGHT_SERVER_IP}:{frc_vision.constants.SERVERS.LIMELIGHT_SERVER_PORT}"
+)
+
 
 aframe = None
 lframe = None
@@ -20,39 +23,42 @@ lframe = None
 def read_astra():
     global aframe
     data = b""
-    payload_size = struct.calcsize("Q")
+    payload_size = struct.calcsize("L")
     while True:
-
-        # Receive size of data
+        # Receive payload with size of data
         while len(data) < payload_size:
             packet = sock.recv(4096)
             if not packet:
                 break
             data += packet
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("Q", packed_msg_size)[0]
-        # data = data[payload_size:]
 
-        # Recieve all incoming frame data
-        # data = data[payload_size:]
+        # Unpack the payload and remove it from the actual frame data
+        packed_msg_size = data[:payload_size]
+        msg_size = struct.unpack("L", packed_msg_size)[0]
+        data = data[payload_size:]
+
+        # Recieve the rest of the incoming frame data
         while len(data) < msg_size:
             data += sock.recv(4096)
 
         # Cut off everything after the size of the data (in case extra data was transmitted)
         frame_data = data[:msg_size]
+        # If any additional data was transmitted
+        # (the next frame, for instance), this will
+        # save that and use it for the next loop.
         data = data[msg_size:]
 
         # Load data into openCV
-        frame = pickle.loads(frame_data)
-        aframe = cv2.resize(frame, (800, 500))
+        aframe = pickle.loads(frame_data)
+        aframe = cv2.resize(aframe, (800, 500))
 
 
 def read_limelight():
     global lframe
     while True:
-        ret, lframe = vcap.read()
-        lframe = cv2.resize(lframe, (800, 500))
+        ret, frame = vcap.read()
+        if ret:
+            lframe = cv2.resize(frame, (800, 500))
 
 
 t1 = threading.Thread(target=read_astra)
@@ -61,14 +67,17 @@ t2 = threading.Thread(target=read_limelight)
 t1.start()
 t2.start()
 
-view_limelight = False
+view_front = False
 while True:
     key = cv2.waitKey(15)
-    if key == 120:
-        view_limelight = not view_limelight
-    if key == 27:  # esc
+    if key == frc_vision.constants.KEYS.CLIENT_SWITCH_KEY:
+        view_front = not view_front
+    if key == frc_vision.constants.KEYS.CV2_WAIT_KEY:  # esc
         break
-    elif view_limelight:  # x
+
+    if view_front:
         cv2.imshow("receive", lframe)
     else:
         cv2.imshow("receive", aframe)
+
+cv2.destroyAllWindows()
